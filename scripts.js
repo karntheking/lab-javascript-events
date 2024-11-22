46fa1a7a074401a999de9b3ae9998411dd580dd2
karntheking <sharmakarn14@gmail.com>
2024-11-22 14:22:57 -0500

    Added event listener for form. Added message when form has been submitted

--- scripts.js
+++ scripts.js
@@ -10,3 +10,9 @@ document.getElementById('hover-btn').addEventListener('mouseout', () => {
 document.getElementById('key-input').addEventListener('keydown', (event) => {
     document.getElementById('key-message').textContent = `You pressed: ${event.key}`;
 });
+
+// Form Events
+document.getElementById('sample-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    document.getElementById('form-message').textContent = "Form submitted!";
+});
